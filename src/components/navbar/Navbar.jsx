c499c39cc10fbecdb58fe3cf49d5d2a772ff8eee
Victoria <victoria.ancalaf@gmail.com>
2020-11-30 16:51:37 -0300

    navlinks

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements'
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavTranslate,
+  NavEnEs,
+} from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-          <NavbarContainer>
-              <NavLogo to='/'>
-                  Victoria
-              </NavLogo>
-          </NavbarContainer>
+        <NavbarContainer>
+          <NavLogo to="/">VA</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">Acerca de mí</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="skills">Habilidades</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="projects">Proyectos</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Contacto</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavTranslate>
+              <NavEnEs>ES | EN</NavEnEs>
+            </NavTranslate>
+        </NavbarContainer>
       </Nav>
     </>
   );
--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -1,40 +1,111 @@
-import styled from 'styled-components';
-import { Link as LinkRouter } from 'react-router-dom'
+import styled from "styled-components";
+import { Link as LinkRouter } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll";
 
 export const Nav = styled.nav`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem;
-    position: sticky;
-    top: 0;
-    z-index: 10;
-    background: #9E7FE7 30%;
-    height: 80px;
-    /* margin-top: -80px;  */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background: rgba(158, 127, 231, 0.3);
+  height: 80px;
+  /* margin-top: -80px;  */
 
-    @media screen and (max-width: 960px) {
-        transition: 0.8s all ease;
-    }
-`
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+`;
 
 export const NavbarContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    height: 80px;
-    z-index: 1;
-    width: 100%;
-    padding: 0 24px;
-    max-width: 1100px;
-`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
 
 export const NavLogo = styled(LinkRouter)`
-    color: #fff;
-    justify-self: flex-start;
+  color: #fbf8ff;
+  justify-self: flex-start;
+  cursor: pointer;
+  font-size: 2.9 rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 24px;
+  text-decoration: none;
+  font-family: "Thasadith", sans-serif;
+  background: #9e7fe7;
+  width: 54px;
+  height: 54px;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
     cursor: pointer;
-    font-size: 1.5 rem;
+    color: #9E7FE7;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavItem = styled.li`
+  height: 80px;
+`;
+
+export const NavLinks = styled(LinkScroll)`
+  display: flex;
+  align-items: center;
+  font-family: "Sarabun", sans-serif;
+  color: #5d576a;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    border-bottom: 3px solid #5d576a;
+  }
+`;
+
+
+export const NavTranslate = styled.nav`
     display: flex;
     align-items: center;
-    margin-left: 24px;
+
+    @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavEnEs = styled.nav`
+    font-family: "Sarabun", sans-serif;
+    font-weight: 200;
+    font-size: 1.25rem;
+    color: #000000;
+    padding: 10px 22px;
+    cursor: pointer;
     text-decoration: none;
 `
